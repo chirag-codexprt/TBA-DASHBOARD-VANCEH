7f2c78c098f99dcffc1528adcc21f632540ce592
Chirag Lathiya <contact@chiraglathiya.com>
2023-01-31 16:13:37 +0530

    Added text based on approve.

--- tba_dashboard/src/components/Document/DocumentTable.js
+++ tba_dashboard/src/components/Document/DocumentTable.js
@@ -144,248 +144,247 @@ const DocumentTable = ({
 								</td>
 								{(obj.allStatus === "pending" ||
 									obj.allStatus === "approved") && (
-										<div>
-											{idArray.includes(obj.id) ? (
-												<Row
-													className='position-absolute'
-													style={{
-														left: "0",
-														bottom: "0",
-														width: "100%",
-													}}>
+									<div>
+										{idArray.includes(obj.id) ? (
+											<Row
+												className='position-absolute'
+												style={{
+													left: "0",
+													bottom: "0",
+													width: "100%",
+												}}>
+												<Col>
+													<Col
+														style={{
+															color: "#B5B6B7",
+														}}>
+														CPF/CNPJ
+													</Col>
+													<Col className=''>
+														<Button
+															className='w-100 p-0'
+															variant='outline-success'>
+															<i class='bi bi-check-lg fs-2'></i>
+															<h6
+																style={{
+																	color: "#C4CCD2",
+																	fontSize:
+																		"11px",
+																}}>
+																Já aprovada,
+																visualizar?
+															</h6>
+														</Button>
+													</Col>
+												</Col>
+												<Col>
+													<Col
+														style={{
+															color: "#B5B6B7",
+														}}>
+														Contrato social
+													</Col>
 													<Col>
-														<Col
-															style={{
-																color: "#B5B6B7",
-															}}>
-															CPF/CNPJ
-														</Col>
-														<Col className=''>
+														{obj?.socialContract ===
+															null && (
 															<Button
-																className='w-100 p-0'
-																variant='outline-success'>
-																<i class='bi bi-check-lg fs-2'></i>
+																className='w-100 p-0 ms-0'
+																onClick={() =>
+																	handleShowImageModal(
+																		obj,
+																		"socialContract"
+																	)
+																}
+																variant='outline-secondary'>
+																<label
+																	style={{
+																		rotate: "45deg",
+																	}}>
+																	<i class='bi bi-paperclip fs-2'></i>
+																</label>
 																<h6
 																	style={{
 																		color: "#C4CCD2",
 																		fontSize:
 																			"11px",
 																	}}>
-																	Já aprovada,
-																	visualizar?
+																	Arraste e
+																	solte aqui
+																	ou importe
+																	dos seus
+																	arquivos
 																</h6>
 															</Button>
-														</Col>
+														)}
+														{obj?.socialContract &&
+															!obj?.socialContract
+																?.approved && (
+																<Button
+																	className='w-100 p-0 ms-0'
+																	onClick={() =>
+																		handleShowImageModal(
+																			obj,
+																			"socialContract"
+																		)
+																	}
+																	variant='outline-warning'>
+																	<i class='bi bi-clock-fill fs-2'></i>
+
+																	<h6
+																		style={{
+																			color: "#C4CCD2",
+																			fontSize:
+																				"11px",
+																		}}>
+																		Aguardando
+																		análise,
+																		visualizar?
+																	</h6>
+																</Button>
+															)}
+
+														{obj?.socialContract &&
+															obj?.socialContract
+																?.approved && (
+																<Button
+																	className='w-100 p-0 ms-0'
+																	onClick={() =>
+																		handleShowImageModal(
+																			obj,
+																			"socialContract"
+																		)
+																	}
+																	variant='outline-success'>
+																	<i class='bi bi-check-lg fs-2'></i>
+
+																	<h6
+																		style={{
+																			color: "#C4CCD2",
+																			fontSize:
+																				"11px",
+																		}}>
+																		Aguardando
+																		análise,
+																		visualizar?
+																	</h6>
+																</Button>
+															)}
+													</Col>
+												</Col>
+												<Col>
+													<Col
+														style={{
+															color: "#B5B6B7",
+														}}>
+														Comprovante de
+														residência
 													</Col>
 													<Col>
-														<Col
-															style={{
-																color: "#B5B6B7",
-															}}>
-															Contrato social
-														</Col>
-														<Col>
-															{obj?.socialContract ===
-																null && (
-																	<Button
-																		className='w-100 p-0 ms-0'
-																		onClick={() =>
-																			handleShowImageModal(
-																				obj,
-																				"socialContract"
-																			)
-																		}
-																		variant='outline-secondary'>
-																		<label
-																			style={{
-																				rotate: "45deg",
-																			}}>
-																			<i class='bi bi-paperclip fs-2'></i>
-																		</label>
-																		<h6
-																			style={{
-																				color: "#C4CCD2",
-																				fontSize:
-																					"11px",
-																			}}>
-																			Aguardando
-																			análise,
-																			visualizar?
-																		</h6>
-																	</Button>
-																)}
-															{obj?.socialContract &&
-																!obj?.socialContract
-																	?.approved && (
-																	<Button
-																		className='w-100 p-0 ms-0'
-																		onClick={() =>
-																			handleShowImageModal(
-																				obj,
-																				"socialContract"
-																			)
-																		}
-																		variant='outline-warning'>
-																		<i class='bi bi-clock-fill fs-2'></i>
+														{obj?.addressProof ===
+															null && (
+															<Button
+																className='w-100 p-0 ms-0'
+																onClick={() =>
+																	handleShowAddressModal(
+																		obj,
+																		"addressProof"
+																	)
+																}
+																variant='outline-secondary'>
+																<label
+																	style={{
+																		rotate: "45deg",
+																	}}>
+																	<i class='bi bi-paperclip fs-2'></i>
+																</label>
+																<h6
+																	style={{
+																		color: "#C4CCD2",
+																		fontSize:
+																			"11px",
+																	}}>
+																	Arraste e
+																	solte aqui
+																	ou importe
+																	dos seus
+																	arquivos
+																</h6>
+															</Button>
+														)}
+														{obj?.addressProof &&
+															!obj?.addressProof
+																?.approved && (
+																<Button
+																	className='w-100 p-0 ms-0'
+																	onClick={() =>
+																		handleShowAddressModal(
+																			obj,
+																			"addressProof"
+																		)
+																	}
+																	variant='outline-warning'>
+																	<i class='bi bi-clock-fill fs-2'></i>
 
-																		<h6
-																			style={{
-																				color: "#C4CCD2",
-																				fontSize:
-																					"11px",
-																			}}>
-																			Aguardando
-																			análise,
-																			visualizar?
-																		</h6>
-																	</Button>
-																)}
+																	<h6
+																		style={{
+																			color: "#C4CCD2",
+																			fontSize:
+																				"11px",
+																		}}>
+																		Aguardando
+																		análise,
+																		visualizar?
+																	</h6>
+																</Button>
+															)}
 
-															{obj?.socialContract &&
-																obj?.socialContract
-																	?.approved && (
-																	<Button
-																		className='w-100 p-0 ms-0'
-																		onClick={() =>
-																			handleShowImageModal(
-																				obj,
-																				"socialContract"
-																			)
-																		}
-																		variant='outline-success'>
-																		<i class='bi bi-check-lg fs-2'></i>
+														{obj?.addressProof &&
+															obj?.addressProof
+																?.approved && (
+																<Button
+																	className='w-100 p-0 ms-0'
+																	onClick={() =>
+																		handleShowAddressModal(
+																			obj,
+																			"addressProof"
+																		)
+																	}
+																	variant='outline-success'>
+																	<i class='bi bi-check-lg fs-2'></i>
 
-																		<h6
-																			style={{
-																				color: "#C4CCD2",
-																				fontSize:
-																					"11px",
-																			}}>
-																			Aguardando
-																			análise,
-																			visualizar?
-																		</h6>
-																	</Button>
-																)}
-														</Col>
+																	<h6
+																		style={{
+																			color: "#C4CCD2",
+																			fontSize:
+																				"11px",
+																		}}>
+																		Aguardando
+																		análise,
+																		visualizar?
+																	</h6>
+																</Button>
+															)}
 													</Col>
-													<Col>
-														<Col
-															style={{
-																color: "#B5B6B7",
-															}}>
-															Comprovante de
-															residência
-														</Col>
-														<Col>
-															{obj?.addressProof ===
-																null && (
-																	<Button
-																		className='w-100 p-0 ms-0'
-																		onClick={() =>
-																			handleShowAddressModal(
-																				obj,
-																				"addressProof"
-																			)
-																		}
-																		variant='outline-secondary'>
-																		<label
-																			style={{
-																				rotate: "45deg",
-																			}}>
-																			<i class='bi bi-paperclip fs-2'></i>
-																		</label>
-																		<h6
-																			style={{
-																				color: "#C4CCD2",
-																				fontSize:
-																					"11px",
-																			}}>
-																			Arraste e
-																			solte aqui
-																			ou importe
-																			dos seus
-																			arquivos
-																		</h6>
-																	</Button>
-																)}
-															{obj?.addressProof &&
-																!obj?.addressProof
-																	?.approved && (
-																	<Button
-																		className='w-100 p-0 ms-0'
-																		onClick={() =>
-																			handleShowAddressModal(
-																				obj,
-																				"addressProof"
-																			)
-																		}
-																		variant='outline-warning'>
-																		<i class='bi bi-clock-fill fs-2'></i>
+												</Col>
 
-																		<h6
-																			style={{
-																				color: "#C4CCD2",
-																				fontSize:
-																					"11px",
-																			}}>
-																			Arraste
-																			e solte
-																			aqui ou
-																			importe
-																			dos seus
-																			arquivos
-																		</h6>
-																	</Button>
-																)}
-
-															{obj?.addressProof &&
-																obj?.addressProof
-																	?.approved && (
-																	<Button
-																		className='w-100 p-0 ms-0'
-																		onClick={() =>
-																			handleShowAddressModal(
-																				obj,
-																				"addressProof"
-																			)
-																		}
-																		variant='outline-success'>
-																		<i class='bi bi-check-lg fs-2'></i>
-
-																		<h6
-																			style={{
-																				color: "#C4CCD2",
-																				fontSize:
-																					"11px",
-																			}}>
-																			Aguardando
-																			análise,
-																			visualizar?
-																		</h6>
-																	</Button>
-																)}
-														</Col>
+												<Row>
+													<Col
+														className='d-flex justify-content-center mt-2 ms-4'
+														style={{
+															color: "#C4CCD2",
+															fontSize: "12px",
+														}}>
+														Responsável por esse
+														cliente: Renata
+														Vasconcelos
 													</Col>
-
-													<Row>
-														<Col
-															className='d-flex justify-content-center mt-2 ms-4'
-															style={{
-																color: "#C4CCD2",
-																fontSize: "12px",
-															}}>
-															Responsável por esse
-															cliente: Renata
-															Vasconcelos
-														</Col>
-													</Row>
 												</Row>
-											) : (
-												""
-											)}
-										</div>
-									)}
+											</Row>
+										) : (
+											""
+										)}
+									</div>
+								)}
 							</tr>
 						))}
 					</tbody>

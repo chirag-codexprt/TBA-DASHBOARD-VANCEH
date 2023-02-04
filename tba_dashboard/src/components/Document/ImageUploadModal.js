6dda750a8fb5a89510e7c45951bc8da0235403bf
YashNaliyapara <yash.naliyapara@codexprt.com>
2023-02-04 16:02:33 +0530

    PDF Review Change

--- tba_dashboard/src/components/Document/ImageUploadModal.js
+++ tba_dashboard/src/components/Document/ImageUploadModal.js
@@ -6,6 +6,7 @@ import { approvedDocumentList } from "../../helper/API/document";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import Loader from "../Loader"
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 const ImageUploadModal = ({
 	open,
@@ -15,6 +16,8 @@ const ImageUploadModal = ({
 	setRefresh,
 }) => {
 	console.log("document.type", document.type);
+
+	const [reload, setReload] = useState(false)
 
 	const hiddenFileInput = useRef(null);
 	const [images, setImages] = useState("");
@@ -60,6 +63,14 @@ const ImageUploadModal = ({
 	function nextPage() {
 		changePage(1);
 	}
+
+	const handleReload = () => {
+		setReload(true)
+		setTimeout(() => {
+			setReload(false)
+		}, 3000);
+	}
+
 	return (
 		<div>
 			<Modal show={open} onHide={handleClose} centered className='zindex'>
@@ -76,13 +87,38 @@ const ImageUploadModal = ({
 							<img src='assets/img/close.png'></img>
 						</Button>
 					</Col>
+					<div>
+						<Button
+							className="border-0"
+							style={{
+								position: "absolute",
+								backgroundColor: "#1C3D59",
+								right: "2%",
+								top: "12%",
+								zIndex: 10000,
+							}}
+							onClick={handleReload}
+						>
+							<i class="bi bi-arrow-clockwise"></i>
+						</Button>
+					</div>
 				</Row>
 				<Row>
 					<Col className='mx-4'>
 						<div
-							className='border  position-relative rounded-2 mb-4'
-							style={{ height: "360px" }}>
-							<>
+							className='border d-flex align-items-center justify-content-center  position-relative rounded-2 mb-4'
+							style={{ height: "400px" }}>
+							{reload ?
+								<div className="d-flex align-items-center justify-content-center h-100 ">
+									<Loader /></div> :
+								<embed
+									src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${imagePreview}`}
+									style={{
+										height: imagePreview ? "100%" : "",
+										width: imagePreview ? "100%" : "",
+										// padding: "0px 15px",
+									}}></embed>}
+							{/* <>
 								<Document
 									file={imagePreview}
 									options={{ workerSrc: "/pdf.worker.js" }}
@@ -107,11 +143,11 @@ const ImageUploadModal = ({
 															carregamento…
 														</span>
 													</div>
-													{/* <div
+													<div
 														className='d-flex justify-content-center align-items-center'
 														height={"100%"}>
 														Página de carregamento…
-													</div> */}
+													</div>
 												</>
 											);
 										}}
@@ -145,7 +181,7 @@ const ImageUploadModal = ({
 										</div>
 									)}
 								</div>
-							</>
+							</> */}
 						</div>
 						<div>
 							<a

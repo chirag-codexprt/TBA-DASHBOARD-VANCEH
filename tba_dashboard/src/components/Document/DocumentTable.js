b8f34fd8fff83c7279f05000e1fd9903d649aba3
Chirag Lathiya <contact@chiraglathiya.com>
2023-02-06 17:12:40 +0530

    Fix height issue in document table documents.

--- tba_dashboard/src/components/Document/DocumentTable.js
+++ tba_dashboard/src/components/Document/DocumentTable.js
@@ -93,29 +93,30 @@ const DocumentTable = ({
 								}}
 								className={
 									idArray.includes(obj.id) &&
-										getRequiredLength(obj) <= 3 &&
-										getRequiredLength(obj) !== 0
+									getRequiredLength(obj) <= 3 &&
+									getRequiredLength(obj) !== 0
 										? "row-height1"
 										: idArray.includes(obj.id) &&
-											getRequiredLength(obj) <= 6 &&
-											getRequiredLength(obj) !== 0
-											? "row-height2"
-											: idArray.includes(obj.id) &&
-												getRequiredLength(obj) >= 14 &&
-												getRequiredLength(obj) !== 0
-												? "row-height"
-												: idArray.includes(obj.id) &&
-													getRequiredLength(obj) >= 6 &&
-													getRequiredLength(obj) <= 9
-													? "row-height3"
-													: idArray.includes(obj.id) &&
-														getRequiredLength(obj) >= 9 &&
-														getRequiredLength(obj) <= 12
-														? "row-height4"
-														: idArray.includes(obj.id) &&
-															getRequiredLength(obj) === 0
-															? "row-height5"
-															: ""
+										  getRequiredLength(obj) >= 4 &&
+										  getRequiredLength(obj) <= 6 &&
+										  getRequiredLength(obj) !== 0
+										? "row-height2"
+										: idArray.includes(obj.id) &&
+										  getRequiredLength(obj) >= 14 &&
+										  getRequiredLength(obj) !== 0
+										? "row-height"
+										: idArray.includes(obj.id) &&
+										  getRequiredLength(obj) >= 6 &&
+										  getRequiredLength(obj) <= 9
+										? "row-height3"
+										: idArray.includes(obj.id) &&
+										  getRequiredLength(obj) >= 9 &&
+										  getRequiredLength(obj) <= 12
+										? "row-height4"
+										: idArray.includes(obj.id) &&
+										  getRequiredLength(obj) === 0
+										? "row-height5"
+										: ""
 									// "row-height"
 								}>
 								<td
@@ -142,56 +143,52 @@ const DocumentTable = ({
 									className='position-relative text-end'
 									style={{ zIndex: 1000 }}>
 									<Button
-										// className='text-white fw-bold p-0'
+										className='text-white fw-bold p-0'
 										style={{
 											width: "100px",
 											fontSize: "12px",
-											borderRadius: "3px",
-											padding: '0px',
-											border: "none",
-											fontWeight: '800'
 										}}
-										className={
+										variant={
 											// 	obj.allStatus === "pending"
 											// 		? "warning"
 											// 		: "success"
 											// }
 											obj.allStatus === "pending"
-												? "document-pending"
+												? "warning"
 												: obj.allStatus === "wait"
-													? "document-wait"
-													: "document-success"
+												? "danger"
+												: "success"
 										}
-									// onClick={
-									// 	obj.allStatus === "pending"
-									// 		? () => handleShowLinkModal(obj)
-									// 		: null
-									// }
+										// onClick={
+										// 	obj.allStatus === "pending"
+										// 		? () => handleShowLinkModal(obj)
+										// 		: null
+										// }
 									>
 										{/* {obj.allStatus === "pending"
 											? "Pendente"
 											: "Concluded"} */}
 
 										{obj.allStatus === "pending"
-											? "Aguard. doc."
+											? "Aguard. rev."
 											: obj.allStatus === "wait"
-												? "Aguard. rev."
-												: "Concluído"}
+											? "Aguard. rev."
+											: "Concluído"}
 									</Button>
 								</td>
 								{(obj.allStatus === "pending" ||
 									obj.allStatus === "wait" ||
 									obj.allStatus === "approved") && (
-										<div>
-											{idArray.includes(obj.id) ? (
-												<Row
-													className='position-absolute'
-													style={{
-														left: "0",
-														bottom: "0",
-														width: "100%",
-													}}>
-													{/* {!obj.allStatus === "wait" && (
+									<div>
+										{idArray.includes(obj.id) ? (
+											<Row
+												className='position-absolute'
+												style={{
+													left: "0",
+													bottom: "0",
+													width: "100%",
+												}}>
+												{/* {!obj.allStatus === "wait" && (
 													<>
 														<TableRowDocument
 															obj={obj}
@@ -210,7 +207,7 @@ const DocumentTable = ({
 													</>
 												)} */}
 
-													{/* {obj.allStatus === "wait" ? (
+												{/* {obj.allStatus === "wait" ? (
 													<GenerateLinkBtn
 														onClick={() =>
 															handleShowLinkModal(
@@ -221,43 +218,43 @@ const DocumentTable = ({
 														md={12}
 													/>
 												) : ( */}
-													<>
-														<TableRowDocument
-															obj={obj}
-															handleShowImageModal={
-																handleShowImageModal
-															}
-														/>
-														<GenerateLinkBtn
-															onClick={() =>
-																handleShowLinkModal(
-																	obj
-																)
-															}
-															obj={obj}
-															md={12}
-														/>
-													</>
-													{/* )} */}
+												<>
+													<TableRowDocument
+														obj={obj}
+														handleShowImageModal={
+															handleShowImageModal
+														}
+													/>
+													<GenerateLinkBtn
+														onClick={() =>
+															handleShowLinkModal(
+																obj
+															)
+														}
+														obj={obj}
+														md={12}
+													/>
+												</>
+												{/* )} */}
 
-													<Row>
-														<Col
-															className='d-flex justify-content-center mt-2 ms-4'
-															style={{
-																color: "#C4CCD2",
-																fontSize: "12px",
-															}}>
-															Responsável por esse
-															cliente: Renata
-															Vasconcelos
-														</Col>
-													</Row>
+												<Row>
+													<Col
+														className='d-flex justify-content-center mt-2 ms-4'
+														style={{
+															color: "#C4CCD2",
+															fontSize: "12px",
+														}}>
+														Responsável por esse
+														cliente: Renata
+														Vasconcelos
+													</Col>
 												</Row>
-											) : (
-												""
-											)}
-										</div>
-									)}
+											</Row>
+										) : (
+											""
+										)}
+									</div>
+								)}
 							</tr>
 						))}
 					</tbody>
--- tba_dashboard/src/components/Document/NewMemberAdd.js
+++ tba_dashboard/src/components/Document/NewMemberAdd.js
@@ -57,7 +57,7 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 		abcCurve: "",
 	});
 
-	const handleFileChange = (acceptedFiles, type, fileName) => {
+	const handleFileChange = (acceptedFiles, type, e) => {
 		console.log("acceptedFiles", acceptedFiles);
 		if (acceptedFiles[0].type !== "application/pdf") {
 			toast.error("Por favor, selecione apenas arquivo pdf");
@@ -67,7 +67,7 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 				setImagePreview(URL.createObjectURL(acceptedFiles[0]));
 				setImages({
 					...images,
-					[fileName]: acceptedFiles[0],
+					[e.target.name]: acceptedFiles[0],
 				});
 			}
 		}
@@ -249,6 +249,8 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 							}
 						})
 						.catch((err) => toast.error("Pedido inválido"));
+				} else {
+					toast.error(res.message);
 				}
 			});
 		}
@@ -262,8 +264,9 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 			aria-labelledby='contained-modal-title-vcenter'
 			centered>
 			<ModalHeader
-				className='border-0 mx-3 mt-2 d-flex justify-content-end fw-bolder'>
-				<Modal.Title id='contained-modal-title-vcenter' style={{ cursor: 'pointer' }} onClick={handleClose}><img src="assets/img/close.png"></img></Modal.Title>
+				className='border-0 mx-3 mt-3 mb-0 fw-bolder fs-6'
+				closeButton>
+				<Modal.Title id='contained-modal-title-vcenter'></Modal.Title>
 			</ModalHeader>
 			<ModalBody className='p-4 pt-0'>
 				<h5 className='fw-bolder'>Criar novo cliente</h5>
@@ -302,7 +305,7 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 					<Col md={6} xs={12}>
 						<Form>
 							<Form.Label className='Doc-Font-Color'>
-								Telefone
+								Email/telefone
 							</Form.Label>
 							<InputGroup className='mb-3 rounded'>
 								<InputGroup.Text
@@ -311,10 +314,10 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 									style={{
 										background: "#F4F6F8",
 									}}>
-									<i class="bi bi-telephone link-icon"></i>
+									<i className='bi bi-envelope-fill link-icon'></i>
 								</InputGroup.Text>
 								<Form.Control
-									placeholder='(00)00000-0000'
+									placeholder='anajuliamarques@tba.com'
 									type='text'
 									name='emailOrPhone'
 									className='Cardinput border-0'
@@ -389,13 +392,13 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 				<div className='d-flex justify-content-end'>
 					<Button
 						onClick={submitDocumentForm}
-						className='mt-4 px-5 p-3 fw-bold border-0'
+						className='mt-4  p-3 px-4 fw-bold border-0'
 						disabled={loading}
 						style={{
 							width: "fit-content",
 							background: "#1C3D59",
 						}}>
-						Criar cliente
+						Criar
 						{loading && (
 							<Spinner
 								animation='grow'
@@ -406,7 +409,7 @@ const NewMemberAdd = ({ show, handleClose, refresh, setRefresh }) => {
 					</Button>
 				</div>
 			</ModalBody>
-		</Modal >
+		</Modal>
 	);
 };
 
